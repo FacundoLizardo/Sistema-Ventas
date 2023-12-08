e4e658d83b206d001e0d83a5b3175e30682b52aa
Lucas Gabriel Tamburlini <74024761+lucastamburlini@users.noreply.github.com>
2023-12-08 17:55:53 -0300

    Agrego archivos de Modelos y diseño modelo de Product completo

--- Api/src/Models/Product.js
+++ Api/src/Models/Product.js
@@ -3,7 +3,7 @@ const { DataTypes } = require("sequelize");
 module.exports = (sequelize) => {
 	sequelize.define("Product",
 		{
-			id: {
+			product_id: {
 				type: DataTypes.UUID,
 				defaultValue: DataTypes.UUIDV4,
 				primaryKey: true,
@@ -12,9 +12,56 @@ module.exports = (sequelize) => {
 			name: {
 				type: DataTypes.STRING,
 				allowNull: false,
+				validate: {
+					len: [0, 50]
+				}
+			},
+			category: {
+				type: DataTypes.STRING,
+				allowNull: false,
+			},
+			cost: {
+				type: DataTypes.DECIMAL(10, 2),
+				allowNull: true,
+			},
+			final_price: {
+				type: DataTypes.DECIMAL(10, 2),
+				allowNull: true,
+			},
+			discount: {
+				type: DataTypes.INTEGER,
+				allowNull: false,
+				validate: {
+					isPositive(value) {
+						if (value < 0) {
+							throw new Error('The discount must be a positive number.');
+						} else if (value > 100) {
+							throw new Error('The discount must be less than 100.')
+						}
+					}
+				}
+			},
+			profit_percentage: {
+				type: DataTypes.INTEGER,
+				allowNull: true,
+			},
+			quantity: {
+				type: DataTypes.INTEGER,
+				allowNull: true,
 			},
 			enabled: {
 				type: DataTypes.BOOLEAN,
+				allowNull: false,
+			},
+			notes_description: {
+				type: DataTypes.STRING,
+				allowNull: true,
+				validate: {
+					len: [0, 255]
+				}
+			},
+			taxes: {
+				type: DataTypes.DECIMAL(10, 2),
 				allowNull: true,
 			},
 		},
