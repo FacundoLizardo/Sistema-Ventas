e6ef01880e6657b6a18724fd5924e67900f2ae98
Lucas Gabriel Tamburlini <74024761+lucastamburlini@users.noreply.github.com>
2023-12-22 09:44:11 -0300

    add navigation

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,17 +1,20 @@
 import "./App.module.css";
 import { Routes, Route } from "react-router-dom";
-import Navbar from "./Components/Navbar/Navbar";
+import Sales from "./Views/Sales/Sales";
+import Stock from "./Views/Stock/Stock";
+import Statistics from "./Views/Statistics/Statistics";
+import Administration from "./Views/Administration/Administration";
+import Layout from "./Views/Layout/Layout";
 
 function App() {
   return (
     <>
-      <Navbar />
       <Routes>
-        <Route path="/" element={""} />
-        <Route path="/" element={""} />
-        <Route path="/" element={""} />
-        <Route path="/" element={""} />
-        <Route path="/" element={""} />
+        <Route path="/" element={<Layout />} />
+        <Route path="/sales" element={<Sales />} />
+        <Route path="/stock" element={<Stock />} />
+        <Route path="/statistics" element={<Statistics />} />
+        <Route path="/administration" element={<Administration />} />
       </Routes>
     </>
   );
