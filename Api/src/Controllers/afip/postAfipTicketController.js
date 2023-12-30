40425f06652db7ebcd0c8cf2c0e4dc5cf18d3352
Lucas Gabriel Tamburlini <74024761+lucastamburlini@users.noreply.github.com>
2023-12-30 11:13:28 -0300

    Se generan las facturas ok

--- Api/src/Controllers/afip/postAfipTicketController.js
+++ Api/src/Controllers/afip/postAfipTicketController.js
@@ -1,35 +1,133 @@
 const Afip = require('@afipsdk/afip.js');
 const afip = new Afip({ CUIT: 20409378472 });
 const path = require('path');
-const htmlPath = path.join(__dirname, 'bill.html')
+const fs = require('fs')
 
-const postAfipTicketController = async () => {
+const postAfipVoucherController = async ({ ptoVta, cbteTipo, concepto, docTipo, docNro, importeGravado, importeExentoIva, importeIva, razonSocial, direccion, cuit, responsableInscripto, iibb, inicioActividad }) => {
 
-    // Descargamos el HTML de ejemplo (ver mas arriba)
-	// y lo guardamos como bill.html
-	const html = require('fs').readFileSync(htmlPath, 'utf8');
-	
-	// Nombre para el archivo (sin .pdf)
-	const name = 'PDF de prueba';
-	
-	// Opciones para el archivo
-	const options = {
-		width: 8, // Ancho de pagina en pulgadas. Usar 3.1 para ticket
-		marginLeft: 0.4, // Margen izquierdo en pulgadas. Usar 0.1 para ticket 
-		marginRight: 0.4, // Margen derecho en pulgadas. Usar 0.1 para ticket 
-		marginTop: 0.4, // Margen superior en pulgadas. Usar 0.1 para ticket 
-		marginBottom: 0.4 // Margen inferior en pulgadas. Usar 0.1 para ticket 
-	};
-	
-	// Creamos el PDF
-	const res = await afip.ElectronicBilling.createPDF({
-		html: html,
-		file_name: name,
-		options: options
-	});
-	
-	// Mostramos la url del archivo creado
-	console.log(res.file);
+	try {
+
+		const lastVoucher = await afip.ElectronicBilling.getLastVoucher(ptoVta, cbteTipo);
+		const numeroFactura = lastVoucher + 1;
+		const fecha = new Date(Date.now() - ((new Date()).getTimezoneOffset() * 60000)).toISOString().split('T')[0];
+
+		let fecha_servicio_desde = null;
+		let fecha_servicio_hasta = null;
+		let fecha_vencimiento_pago = null;
+
+		if (concepto === 2 || concepto === 3) {
+			fecha_servicio_desde = 20191213;
+			fecha_servicio_hasta = 20191213;
+			fecha_vencimiento_pago = 20191213;
+		}
+
+
+		const data = {
+			'CantReg': 1,
+			'PtoVta': ptoVta,
+			'CbteTipo': cbteTipo,
+			'Concepto': concepto,
+			'DocTipo': docTipo,
+			'DocNro': docNro,
+			'CbteDesde': numeroFactura,
+			'CbteHasta': numeroFactura,
+			'CbteFch': parseInt(fecha.replace(/-/g, '')),
+			'FchServDesde': fecha_servicio_desde,
+			'FchServHasta': fecha_servicio_hasta,
+			'FchVtoPago': fecha_vencimiento_pago,
+			'ImpTotal': importeGravado + importeIva + importeExentoIva,
+			'ImpTotConc': 0,
+			'ImpNeto': importeGravado,
+			'ImpOpEx': importeExentoIva,
+			'ImpIVA': importeIva,
+			'ImpTrib': 0,
+			'MonId': 'PES',
+			'MonCotiz': 1,
+			'Iva': [
+				{
+					'Id': 5,
+					'BaseImp': importeGravado,
+					'Importe': importeIva
+				}
+			],
+
+			"razonSocial": razonSocial,
+			"direccion": direccion,
+			"cuit ": cuit,
+			"responsableInscripto": responsableInscripto,
+			"iibb": iibb,
+			"inicioActividad": inicioActividad
+
+		};
+
+		console.log("Valores de data: ", data);
+
+		const voucherData = await afip.ElectronicBilling.createVoucher(data);
+
+		return { voucherData, data };
+
+	} catch (error) {
+		console.error('Error en postAfipVoucherController:', error);
+		throw error;
+	}
 }
 
-module.exports = postAfipTicketController
+const postAfipTicketController = async (voucherData, data) => {
+	try {
+		const htmlPath = path.join(__dirname, 'bill.html');
+		let html = fs.readFileSync(htmlPath, 'utf8');
+
+		const replacedHTML = html
+			.replace('{{CAE}}', voucherData.CAE || "")
+			.replace('{{Vencimiento}}', voucherData.CAEFchVto || "")
+			.replace("{{razonSocial}}", data.razonSocial)
+			.replace("{{direccion}}", data.direccion)
+			.replace("{{cuit}}", data.cuit)
+			.replace("{{responsableInscripto}}", data.responsableInscripto)
+			.replace("{{iibb}}", data.iibb)
+			.replace("{{inicioActividad}}", data.inicioActividad)
+
+
+		const options = {
+			width: 8,
+			marginLeft: 0.4,
+			marginRight: 0.4,
+			marginTop: 0.4,
+			marginBottom: 0.4
+		};
+
+		const pdfData = await afip.ElectronicBilling.createPDF({
+			html: replacedHTML,
+			file_name: 'Voucher',
+			options: options
+		});
+
+		console.log(pdfData);
+
+		/* if (pdfData && pdfData.content) {
+			fs.writeFileSync('voucher.pdf', pdfData.content, 'utf8');
+			console.log('PDF del voucher generado exitosamente');
+			return pdfData;
+		} else {
+			throw new Error('El contenido del PDF es indefinido o nulo');
+		} */
+	} catch (error) {
+		console.error('Ocurrió un error al generar el PDF:', error);
+		throw new Error(error);
+	}
+};
+
+
+const emitVoucherAndGeneratePDF = async ({ ptoVta, cbteTipo, concepto, docTipo, docNro, importeGravado, importeExentoIva, importeIva, razonSocial, direccion, cuit, responsableInscripto, iibb, inicioActividad }) => {
+	try {
+		const { voucherData, data } = await postAfipVoucherController({ ptoVta, cbteTipo, concepto, docTipo, docNro, importeGravado, importeExentoIva, importeIva, razonSocial, direccion, cuit, responsableInscripto, iibb, inicioActividad });
+		await postAfipTicketController(voucherData, data);
+	} catch (error) {
+		console.error('Ocurrió un error:', error);
+	}
+};
+
+
+
+
+module.exports = emitVoucherAndGeneratePDF
--- Api/src/Handlers/afipHandler.js
+++ Api/src/Handlers/afipHandler.js
@@ -1,8 +1,9 @@
-const postAfipTicketController = require("../Controllers/afip/postAfipTicketController");
+const emitVoucherAndGeneratePDF = require("../Controllers/afip/postAfipTicketController");
 
 const postAfip = async (req, res) => {
+    const { ptoVta, cbteTipo, concepto, docTipo, docNro, importeGravado, importeExentoIva, importeIva, razonSocial, direccion, cuit, responsableInscripto, iibb, inicioActividad } = req.body
     try {
-        const afipInvoice = postAfipTicketController();
+        const afipInvoice = await emitVoucherAndGeneratePDF({ ptoVta, cbteTipo, concepto, docTipo, docNro, importeGravado, importeExentoIva, importeIva, razonSocial, direccion, cuit, responsableInscripto, iibb, inicioActividad });
         return res.status(200).json({ success: true, afipInvoice })
     } catch (error) {
         return res.status(400).json({ error: error.message })
