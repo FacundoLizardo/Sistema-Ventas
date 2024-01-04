2041f3fc2706bf3dfeffca5c81042012c1fcfc24
FacundoLizardo <facundolizardo75@gmail.com>
2024-01-04 09:00:08 -0300

    ejemplo de usuario para testeo de ruta agregado en postUserController

--- Api/src/Controllers/user/postUserController.js
+++ Api/src/Controllers/user/postUserController.js
@@ -35,4 +35,17 @@ const postUserController = async (
 	}
 };
 
+//////////////////////////// modelo de usario para probar ruta /////////////////////////////
+// {
+//     "firstName": "John",
+//     "lastName": "Doe",
+//     "email": "john.doe@example.com",
+//     "address": "123 Main St",
+//     "phoneNumber": "555-1234",
+//     "cuit": "123456789",
+//     "branch": ["e4bf1f1d-0a33-4c02-a4e4-4a97cd3405d2", "f4bf1f1d-0a33-4c02-a4e4-4a97cd3405d2"],
+//     "enabled": true,
+//     "role": "admin"
+//   }
+
 module.exports = postUserController;
