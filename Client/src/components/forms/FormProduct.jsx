e3daa4ea8b8026bea9ad776e1336dea882da854c
Lucas Gabriel Tamburlini <74024761+lucastamburlini@users.noreply.github.com>
2024-01-15 16:58:39 -0300

    arreglo nav

--- Client/src/components/forms/FormProduct.jsx
+++ Client/src/components/forms/FormProduct.jsx
@@ -16,51 +16,101 @@ const FormProducts = () => {
     taxes: 0,
     barcode: "",
   });
+  console.log(dataProduct);
+
+  const handleData = (e) => {
+    const { name, value } = e.target;
+    setDataProduct((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
 
   return (
     <form className={style.formContainer}>
+      {/* Sección 1: Nombre y Categoría */}
       <div className={style.itemContainer}>
         <div className={style.item}>
-          <label for="name">Nombre</label>
-          <input type="text" name="name" id="name" />
+          <label htmlFor="name">Nombre</label>
+          <input
+            type="text"
+            name="name"
+            id="name"
+            autoComplete="name"
+            onChange={handleData}
+          />
         </div>
         <div className={style.item}>
-          <label for="category">Categoría</label>
-          <input type="text" name="category" id="category" />
+          <label htmlFor="category">Categoría</label>
+          <input
+            type="text"
+            name="category"
+            id="category"
+            autoComplete="category"
+          />
         </div>
       </div>
 
+      {/* Sección 2: Costo, Impuestos, Descuento, Precio Final */}
       <div className={style.itemContainer}>
-      <div className={style.item}>
-          <label for="category">Costo</label>
-          <input type="text" name="category" id="category" />
+        <div className={style.item}>
+          <label htmlFor="cost">Costo</label>
+          <input type="number" name="cost" id="cost" autoComplete="cost" />
         </div>
         <div className={style.item}>
-          <label for="category">Descuento</label>
-          <input type="text" name="category" id="category" />
+          <label htmlFor="taxes">Impuestos</label>
+          <input type="number" name="taxes" id="taxes" autoComplete="taxes" />
         </div>
         <div className={style.item}>
-          <label for="category">Precio final</label>
-          <input type="text" name="category" id="category" />
+          <label htmlFor="discount">Descuento</label>
+          <input
+            type="number"
+            name="discount"
+            id="discount"
+            autoComplete="discount"
+          />
+        </div>
+        <div className={style.item}>
+          <p>Precio Final</p>
+          <p>{dataProduct.finalPrice}</p>
         </div>
       </div>
 
+      {/* Sección 3: Código de Barras y Disponibilidad */}
       <div className={style.itemContainer}>
-      
         <div className={style.item}>
-          <label for="category">Categoría</label>
-          <input type="text" name="category" id="category" />
+          <label htmlFor="barcode">Código de barras</label>
+          <input
+            type="text"
+            name="barcode"
+            id="barcode"
+            autoComplete="barcode"
+          />
+        </div>
+        <div className={style.item}>
+          <label htmlFor="enabled">Disponible</label>
+          <select
+            type="text"
+            name="enabled"
+            id="enabled"
+            autoComplete="enabled"
+          >
+            <option value="true">Habilitado</option>
+            <option value="false">Deshabilitado</option>
+          </select>
         </div>
       </div>
 
+      {/* Sección 4: Descripción */}
       <div className={style.itemContainer}>
         <div className={style.item}>
-          <label for="name">Disponible</label>
-          <input type="checkbox" name="enabled" id="enabled" />
-        </div>
-        <div className={style.item}>
-          <label for="category">Categoría</label>
-          <input type="text" name="category" id="category" />
+          <label htmlFor="notesDescription">Descripción</label>
+          <textarea
+            type="text"
+            name="notesDescription"
+            id="notesDescription"
+            autoComplete="notesDescription"
+          />
         </div>
       </div>
     </form>
--- Client/src/components/navbar/Navbar.jsx
+++ Client/src/components/navbar/Navbar.jsx
@@ -1,29 +1,28 @@
 import { NavLink } from "react-router-dom";
 import style from "./Navbar.module.css";
-import LoginButton from "../buttons/LoginButton/LoginButton";
-import LogOutButton from "../buttons/LogOutButton/LogOutButton";
+
 const Navbar = () => {
 	return (
 		<div className={style.navbarContainer}>
 			<nav>
 				<ul>
 					<li>
-						<NavLink exact to="/" activeClassName={style.activeLink}>
+						<NavLink to="/" activeclassname={style.activeLink}>
 							Ventas
 						</NavLink>
 					</li>
 					<li>
-						<NavLink to="/stock" activeClassName={style.activeLink}>
+						<NavLink to="/stock" activeclassname={style.activeLink}>
 							Stock
 						</NavLink>
 					</li>
 					<li>
-						<NavLink to="/statistics" activeClassName={style.activeLink}>
+						<NavLink to="/statistics" activeclassname={style.activeLink}>
 							Estadísticas
 						</NavLink>
 					</li>
 					<li>
-						<NavLink to="/administration" activeClassName={style.activeLink}>
+						<NavLink to="/administration" activeclassname={style.activeLink}>
 							Administration
 						</NavLink>
 					</li>
