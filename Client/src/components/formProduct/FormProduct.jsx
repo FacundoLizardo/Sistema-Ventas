0ed77f9e98bbc48634fe0580f5dc2c0e4b22d522
Lucas Gabriel Tamburlini <74024761+lucastamburlini@users.noreply.github.com>
2024-01-15 12:43:39 -0300

    Avances en el form de productos

--- Client/src/components/formProduct/FormProduct.jsx
+++ Client/src/components/formProduct/FormProduct.jsx
@@ -1,10 +1,39 @@
+/* eslint-disable react/no-unknown-property */
+import { useState } from "react";
 import style from "./FormProduct.module.css";
 
 const FormProducts = () => {
+  const [dataProduct, setDataProduct] = useState({
+    name: "",
+    category: "",
+    cost: 0,
+    finalPrice: 0,
+    discount: 0,
+    profitPercentage: 30,
+    stock: 70,
+    enabled: true,
+    notesDescription: "",
+    taxes: 0,
+    barcode: "",
+  });
+
   return (
-    <section className={style.formProductsContainer}>
-      <div>Esto es el form de products</div>
-    </section>
+    <form className={style.formProductsContainer}>
+      <div className={style.itemContainer}>
+        <div className={style.item}>
+          <label for="name">Nombre</label>
+          <input type="text" name="name" id="name" />
+        </div>
+        <div className={style.item}>
+          <label for="category">Categoría</label>
+          <input type="text" name="category" id="category" />
+        </div>
+        <div className={style.item}>
+          <label for="name">Disponible</label>
+          <input type="checkbox" name="enabled" id="enabled" />
+        </div>
+      </div>
+    </form>
   );
 };
 
