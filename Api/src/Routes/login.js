9de70c67112bd193dbadc5e4111a5efccf2632c6
FacundoLizardo <facundolizardo75@gmail.com>
2024-01-15 13:31:46 -0300

    ruta login funcionando

---
+++ Api/src/Routes/login.js
@@ -0,0 +1,8 @@
+const { Router } = require("express");
+const { loginHandler } = require("../Handlers/loginHandler");
+
+const router = Router();
+
+router.get("/", loginHandler);
+
+module.exports = router;
