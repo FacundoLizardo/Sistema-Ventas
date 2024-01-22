360d87dca108cec7c6667ee5dfc05d9e72794b5a
FacundoLizardo <facundolizardo75@gmail.com>
2024-01-22 12:58:01 -0300

    coreccion de informacion de usuario

--- Api/src/Controllers/user/getUserByEmailAndPassword.js
+++ Api/src/Controllers/user/getUserByEmailAndPassword.js
@@ -5,7 +5,18 @@ const getUserByEmailAndPasswordController = async ({ email, password }) => {
 		const user = await User.findOne({
 			where: { email: email },
 		});
-
+		const { branch, cuit, enabled, firstName, lastName, phoneNumber, role } =
+			user;
+		const data = {
+			firstName,
+			lastName,
+			branch,
+			email,
+			cuit,
+			enabled,
+			phoneNumber,
+			role,
+		};
 		if (!user) {
 			throw new Error(`User whit the email: ${email} does not exist.`);
 		}
@@ -14,7 +25,7 @@ const getUserByEmailAndPasswordController = async ({ email, password }) => {
 			throw new Error("Wrong password, please try again.");
 		}
 		if (user && user.password === password) {
-			return user;
+			return data;
 		}
 	} catch (error) {
 		throw new Error(`Error retrieving user: ${error.message}`);
