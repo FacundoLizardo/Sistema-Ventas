2cd682290ead03c7eedc37a39c59b9f13e378acb
FacundoLizardo <facundolizardo75@gmail.com>
2024-04-03 00:38:40 -0300

    react icons installed on client

--- Client/src/context/products/productsContext.jsx
+++ Client/src/context/products/productsContext.jsx
@@ -1,30 +1,29 @@
 import axios from "axios";
-import { createContext, useState, useEffect } from "react";
+import {createContext, useState, useEffect} from "react";
 
 const ProductContext = createContext();
 
-const ProductProvider = ({ children }) => {
-	const [products, setProducts] = useState([]);
-	const URL = import.meta.env.VITE_URL_BACKEND;
+const ProductProvider = ({children}) => {
+    const [products, setProducts] = useState([]);
 
-	useEffect(() => {
-		const fetchData = async () => {
-			try {
-				const { data } = await axios.get(`${URL}/products`);
-				setProducts(data);
-			} catch (error) {
-				console.error("Error fetching data:", error.message);
-			}
-		};
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const {data} = await axios.get(`/products`);
+                setProducts(data);
+            } catch (error) {
+                console.error("Error fetching data:", error.message);
+            }
+        };
 
-		fetchData();
-	}, []);
+        fetchData();
+    }, []);
 
-	return (
-		<ProductContext.Provider value={{ products, setProducts }}>
-			{children}
-		</ProductContext.Provider>
-	);
+    return (
+        <ProductContext.Provider value={{products, setProducts}}>
+            {children}
+        </ProductContext.Provider>
+    );
 };
 
-export { ProductContext, ProductProvider };
+export {ProductContext, ProductProvider};
--- Client/src/main.jsx
+++ Client/src/main.jsx
@@ -6,7 +6,7 @@ import { UserProvider } from "./context/user/userContext.jsx";
 import CartProvider from "./context/cart/cart.jsx";
 import axios from "axios";
 
-axios.defaults.baseURL = "http://localhost:3002";
+axios.defaults.baseURL = import.meta.env.VITE_URL_BACKEND;
 
 ReactDOM.createRoot(document.getElementById("root")).render(
 	<UserProvider>
