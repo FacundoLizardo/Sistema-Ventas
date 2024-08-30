438430fd5ca630ce961e3c1f77c88a2e25d1c710
Facundo Lizardo <facundolizardo75@gmail.com>
2024-08-30 11:33:28 -0300

    login view

---
+++ frontend/src/app/[companyId]/page.tsx
@@ -0,0 +1,9 @@
+export default function RootPage() {
+    return (
+        //todo hacer un check para ver si el id del path es un id de una company
+      <main>
+        companyId
+        
+      </main>
+    );
+  }
