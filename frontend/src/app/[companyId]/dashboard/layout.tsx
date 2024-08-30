df6fffa79b46517831ec99f7af77f14e613d54cc
facundolizardo75@gmail.com <facundolizardo75@gmail.com>
2024-08-30 15:38:17 -0300

    routes structure

---
+++ frontend/src/app/[companyId]/dashboard/layout.tsx
@@ -0,0 +1,9 @@
+// aca va la side bar 
+
+export default function SideBar() {
+    return (
+      <main>
+        
+      </main>
+    );
+  }
---
+++ frontend/src/app/admin/page.tsx
@@ -0,0 +1,9 @@
+export default function Page() {
+    return (
+        //dashboar del superadmin 
+        //esta vista se va a mostrar si tenes permiso de superadmin 
+      <main>
+        
+      </main>
+    );
+  }
