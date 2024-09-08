916edf1d702ee50ffb0b47610ac2dbe357c3a5df
facundolizardo75@gmail.com <facundolizardo75@gmail.com>
2024-09-07 23:17:21 -0300

    create branch form

--- frontend/src/app/[locale]/admin/page.tsx
+++ frontend/src/app/[locale]/admin/page.tsx
@@ -1,3 +1,4 @@
+import CreateBranchForm from "@/components/admin/branches/CreateBranchForm";
 import CreateCompanyForm from "@/components/admin/companyFroms/CreateCompanyForm";
 import Dashboard from "@/components/admin/Dashboard";
 import CreateUserForm from "@/components/admin/userForms/CreateUserForm";
@@ -5,15 +6,27 @@ import CreateUserForm from "@/components/admin/userForms/CreateUserForm";
 export default function Page() {
   const companyId = "1ae9cc57-df39-48fc-be22-46b02a0632a1";
 
+  const users = [
+    { id: "123e4567-e89b-12d3-a456-426614174000", name: "Juan Pérez" },
+    { id: "123e4567-e89b-12d3-a456-426614174001", name: "María García" },
+    { id: "123e4567-e89b-12d3-a456-426614174002", name: "Carlos López" },
+    { id: "123e4567-e89b-12d3-a456-426614174003", name: "Ana Martínez" },
+    { id: "123e4567-e89b-12d3-a456-426614174004", name: "Luis Rodríguez" },
+  ];
+
   return (
     // Dashboard del superadmin
     // Esta vista se va a mostrar si tienes permiso de superadmin
+    // Hacer dashboard lista de compañias y al seleccionar una 
+    //se va a la vista de esa compañia [companyId]
+    //en esa vista se veran los usuarios asociados y branches
 
 
     <main>
       {/* <Dashboard /> */}
       {/* <CreateUserForm companyId={companyId}/> */}
-      <CreateCompanyForm />
+      {/* <CreateCompanyForm /> */}
+          <CreateBranchForm companyId={companyId} users={users}/>
     </main>
   );
 }
