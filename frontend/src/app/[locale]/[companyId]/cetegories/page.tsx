1cd2cf6faafaeb43d5fafc8d5683d32d00c7af10
lucastamburlini <lucastamburlini@gmail.com>
2024-10-01 17:29:17 -0300

    Probando otra cosa

--- frontend/src/app/[locale]/[companyId]/cetegories/page.tsx
+++ frontend/src/app/[locale]/[companyId]/cetegories/page.tsx
@@ -2,6 +2,44 @@ import CategoriesContainer from "@/components/categories/CategoriesContainer";
 import CategoriesServices from "@/services/cetegories/CategoriesServices";
 import SubCategoriesServices from "@/services/subCetegories/SubCategoriesServices";
 
+const fetchCategoriesData = async ({ companyId }: { companyId: string }) => {
+  try {
+    const [categoriesResponse, subCategoriesResponse] = await Promise.all([
+      CategoriesServices.get({ companyId }),
+      SubCategoriesServices.get({ companyId }),
+    ]);
+
+    return {
+      categories: categoriesResponse.categories.map(
+        (category: { name: string; id: string; description?: string }) => ({
+          name: category.name,
+          id: category.id,
+          description: category.description,
+        })
+      ),
+      subCategories: subCategoriesResponse.subCategories.map(
+        (subCategory: {
+          name: string;
+          id: string;
+          categoryId: string;
+          description?: string;
+        }) => ({
+          name: subCategory.name,
+          id: subCategory.id,
+          description: subCategory.description,
+          categoryId: subCategory.categoryId,
+        })
+      ),
+    };
+  } catch (error) {
+    console.error("Error fetching categories or subcategories:", error);
+    return {
+      categories: [],
+      subCategories: [],
+    };
+  }
+};
+
 export default async function Page({
   params: { companyId },
 }: {
@@ -10,31 +48,19 @@ export default async function Page({
     companyId: string;
   };
 }) {
-  const [categoriesData, subCategoriesData] = await Promise.all([
-    CategoriesServices.get({ companyId }),
-    SubCategoriesServices.get({ companyId }),
-  ]);
+  // Función para manejar las solicitudes y optimizar errores
 
-  const categories = categoriesData.categories.map(
-    (category: { name: string; id: string, description?: string }) => ({
-      name: category.name,
-      id: category.id,
-      description: category.description,
-    })
-  );
-
-  const subCategories = subCategoriesData.subCategories.map(
-    (subCategory: { name: string; id: string, categoryId: string, description?: string }) => ({
-      name: subCategory.name,
-      id: subCategory.id,
-      description: subCategory.description,
-      categoryId: subCategory.categoryId,
-    })
-  );
+  const { categories, subCategories } = await fetchCategoriesData({
+    companyId,
+  });
 
   return (
     <main className="grid gap-4">
-      <CategoriesContainer categories={categories} subCategories={subCategories} companyId={companyId} />
+      <CategoriesContainer
+        categories={categories}
+        subCategories={subCategories}
+        companyId={companyId}
+      />
     </main>
   );
 }
