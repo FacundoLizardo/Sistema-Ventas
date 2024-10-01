b1786e908df65ed185dd2eb2312bb15a74089f47
lucastamburlini <lucastamburlini@gmail.com>
2024-10-01 17:26:28 -0300

    volviendo atras category

--- frontend/src/app/[locale]/[companyId]/cetegories/page.tsx
+++ frontend/src/app/[locale]/[companyId]/cetegories/page.tsx
@@ -2,65 +2,39 @@ import CategoriesContainer from "@/components/categories/CategoriesContainer";
 import CategoriesServices from "@/services/cetegories/CategoriesServices";
 import SubCategoriesServices from "@/services/subCetegories/SubCategoriesServices";
 
-const fetchCategoriesData = async (companyId: string) => {
+export default async function Page({
+  params: { companyId },
+}: {
+  params: {
+    locale: string;
+    companyId: string;
+  };
+}) {
   const [categoriesData, subCategoriesData] = await Promise.all([
     CategoriesServices.get({ companyId }),
     SubCategoriesServices.get({ companyId }),
   ]);
-  return { categoriesData, subCategoriesData };
-};
-
-export default async function Page({
-  params: { companyId },
-}: {
-  params: { companyId: string };
-}) {
-  const { categoriesData, subCategoriesData } = await fetchCategoriesData(
-    companyId
-  );
 
   const categories = categoriesData.categories.map(
-    ({
-      name,
-      id,
-      description,
-    }: {
-      name: string;
-      id: string;
-      description: string;
-    }) => ({
-      name,
-      id,
-      description,
+    (category: { name: string; id: string, description?: string }) => ({
+      name: category.name,
+      id: category.id,
+      description: category.description,
     })
   );
 
   const subCategories = subCategoriesData.subCategories.map(
-    ({
-      name,
-      id,
-      categoryId,
-      description,
-    }: {
-      name: string;
-      id: string;
-      categoryId: string;
-      description: string;
-    }) => ({
-      name,
-      id,
-      description,
-      categoryId,
+    (subCategory: { name: string; id: string, categoryId: string, description?: string }) => ({
+      name: subCategory.name,
+      id: subCategory.id,
+      description: subCategory.description,
+      categoryId: subCategory.categoryId,
     })
   );
 
   return (
     <main className="grid gap-4">
-      <CategoriesContainer
-        categories={categories}
-        subCategories={subCategories}
-        companyId={companyId}
-      />
+      <CategoriesContainer categories={categories} subCategories={subCategories} companyId={companyId} />
     </main>
   );
 }
